20c2ea406e88a742f2fdc917d845c31bde0b6532
bhag773 <gs05268348@priestley.ac.uk>
2025-10-02 13:12:52 +0100

    Add files via upload

---
+++ Python template.py
@@ -0,0 +1,16 @@
+#(name of the program)
+#(your name )
+#(date)
+#OCR computer science
+
+def three():
+    pass
+def two():
+    pass
+def one():
+    pass
+
+def main():
+    pass
+if (__name__ == "__main__"):
+    main()
